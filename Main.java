30706fe3c299e90c0b5327ea1ce571f038419caa
BiOksana <biokissme@gmail.com>
2024-05-17 15:01:07 +0200

    Added third way of output

--- Main.java
+++ Main.java
@@ -14,10 +14,19 @@ public class Main {
 
         System.out.println("Firs variant (tabular format): ");
         System.out.println("Name:\t" + name);
-        System.out.println("Age:\t" + age);
         System.out.println("City:\t" + placeOfResidence);
+        System.out.println("Age:\t" + age);
         System.out.println("Hobby:\t" + myHobby);
+
         System.out.println("Second variant (string format): ");
-        System.out.println("Man called " + name + " lives in town " + placeOfResidence + ". He/ is " + age + " and he loves " + myHobby + ".");
+        System.out.println("Man called " + name + " lives in town "
+                + placeOfResidence + ". He is "
+                + age + " and he loves " + myHobby + ".");
+
+        System.out.println("Third variant (another): ");
+        System.out.println("Name - " + name);
+        System.out.println("City - " + placeOfResidence);
+        System.out.println("Age - " + age);
+        System.out.println("Hobby - " + myHobby);
     }
 }
